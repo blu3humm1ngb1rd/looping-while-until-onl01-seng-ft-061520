22132f2ea8d64ada0e91815560e19f97a4cb7c12
BJ Lilley <bethjeanlilley@gmail.com>
2020-06-16 21:36:58 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,7 +1,7 @@
 def using_until
   levitation_force = 6
   #until lev is 10 put phrase 
-  until levitation_force <= 10 
+  until levitation_force == 10 
   puts "Wingardium Leviosa"
   levitation_force += 1
   end
